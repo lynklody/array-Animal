bc45b4466064fbd6dc90fff55a982e0c604ba58a
lynklody <liuai_1995@live.cn>
2019-11-18 06:04:28 -0800

    change in homework

--- Driver.java
+++ Driver.java
@@ -4,8 +4,12 @@ public class Driver {
         Animal dog = new Animal("Dog", "mammal", 4, true);
         Animal fish = new Animal("Fish", "aquatic", 0, false);
         Animal bird = new Animal("Bird", "avian", 2, true);
-        // INSERT YOUR CODE HERE
-        // Create an array named animals for the “dog”, “fish” and “bird” instances defined
+        // ################################### HOMEWORK #######################################
+        // 1. Create 2 more animals with criterion shown below and add to array animals:
+        //    { frog: amphibian, 4 legs, no fur }
+        //    { plankton: aquatic, no leg, no fur }
+        // 2. Write a function named find_swimmers to add aquatic and amphibian animals to an array named swimmers 
+        // ############################### INSERT YOUR CODE HERE ##############################
 
     }
 }
